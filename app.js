3323c844ada0e55f38fb213d6e4081844e7f398a
stsvt <taras.stasiv3@gmail.com>
2026-01-20 19:38:54 +0200

    feat: add global error handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -9,7 +12,16 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
+app.set('query parser', 'extended');
+
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Hello World!' });
 });
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
 module.exports = app;
