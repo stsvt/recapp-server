896c6f6a762b47d2e820d11bfbe49efd1616ebed
stsvt <taras.stasiv3@gmail.com>
2026-02-09 13:37:06 +0200

    fix: temporary rid of middleware

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+// const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
+// const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const setupSwagger = require('./utils/swagger');
@@ -40,9 +40,9 @@ app.use(cors({ origin: process.env.ORIGIN_URL, credentials: true }));
 
 app.use(express.json({ limit: '10kb' }));
 
-app.use(mongoSanitize());
-app.use(xss());
-app.use(hpp());
+// app.use(mongoSanitize());
+// app.use(xss());
+// app.use(hpp());
 
 app.set('query parser', 'extended');
 
