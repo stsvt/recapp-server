e325be83e8a4452b2a241ee9d6f9de8d957c523c
hvlvk <vikagevel19@gmail.com>
2026-05-14 00:50:44 +0300

    feat: implement weighted hybrid recomendation

--- routes/collaborationFilteringRoutes.js
+++ routes/collaborationFilteringRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const collaborationFilteringController = require('../controllers/collaborationFilteringController');
+const authController = require('../controllers/authController');
 
 router.get('/item-based', collaborationFilteringController.getSimilarMovies);
 router.get(
   '/user-based',
+  authController.protect,
   collaborationFilteringController.getPersonalizedRecommendations,
 );
 
